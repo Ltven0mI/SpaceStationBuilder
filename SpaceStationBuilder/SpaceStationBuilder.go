af72accf743de4392e5c3bb112b5a4e1c0d0535b
Ltven0mI <lt.ven0mi@gmail.com>
2016-05-21 16:05:37 +1000

    Now draws line to cursor

--- SpaceStationBuilder/SpaceStationBuilder.go
+++ SpaceStationBuilder/SpaceStationBuilder.go
@@ -6,18 +6,18 @@ import (
 	"os"
 )
 
-var winTitle string = "Go-SDL2 Render"
+var winTitle string = "Space Station Builder"
 var winWidth, winHeight int = 800, 600
 
 func run() int {
 	var window *sdl.Window
 	var renderer *sdl.Renderer
-	var points []sdl.Point
-	var rect sdl.Rect
-	var rects []sdl.Rect
+	var event sdl.Event
+	var running bool
+	var mx, my int = 0, 0
 
 	window, err := sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		winWidth, winHeight, sdl.WINDOW_SHOWN)
+		winWidth, winHeight, sdl.WINDOW_RESIZABLE)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create window: %s\n", err)
 		return 1
@@ -31,37 +31,39 @@ func run() int {
 	}
 	defer renderer.Destroy()
 
-	renderer.Clear()
+	running = true
+	for running {
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			case *sdl.MouseMotionEvent:
+				fmt.Printf("[%d ms] MouseMotion\ttype:%d\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n",
+					t.Timestamp, t.Type, t.Which, t.X, t.Y, t.XRel, t.YRel)
+				mx = int(t.X)
+				my = int(t.Y)
+			case *sdl.MouseButtonEvent:
+				fmt.Printf("[%d ms] MouseButton\ttype:%d\tid:%d\tx:%d\ty:%d\tbutton:%d\tstate:%d\n",
+					t.Timestamp, t.Type, t.Which, t.X, t.Y, t.Button, t.State)
+			case *sdl.MouseWheelEvent:
+				fmt.Printf("[%d ms] MouseWheel\ttype:%d\tid:%d\tx:%d\ty:%d\n",
+					t.Timestamp, t.Type, t.Which, t.X, t.Y)
+			case *sdl.KeyUpEvent:
+				fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
+					t.Timestamp, t.Type, t.Keysym.Sym, t.Keysym.Mod, t.State, t.Repeat)
+			}
+		}
 
-	renderer.SetDrawColor(255, 255, 255, 255)
-	renderer.DrawPoint(150, 300)
+		renderer.SetDrawColor(0, 0, 0, 255)
+		renderer.Clear()
 
-	renderer.SetDrawColor(0, 0, 255, 255)
-	renderer.DrawLine(0, 0, 200, 200)
+		renderer.SetDrawColor(255, 0, 0, 255)
+		renderer.DrawLine(0, 0, mx, my)
 
-	points = []sdl.Point{{0, 0}, {100, 300}, {100, 300}, {200, 0}}
-	renderer.SetDrawColor(255, 255, 0, 255)
-	renderer.DrawLines(points)
+		renderer.Present()
 
-	rect = sdl.Rect{300, 0, 200, 200}
-	renderer.SetDrawColor(255, 0, 0, 255)
-	renderer.DrawRect(&rect)
-
-	rects = []sdl.Rect{{400, 400, 100, 100}, {550, 350, 200, 200}}
-	renderer.SetDrawColor(0, 255, 255, 255)
-	renderer.DrawRects(rects)
-
-	rect = sdl.Rect{250, 250, 200, 200}
-	renderer.SetDrawColor(0, 255, 0, 255)
-	renderer.FillRect(&rect)
-
-	rects = []sdl.Rect{{500, 300, 100, 100}, {200, 300, 200, 200}}
-	renderer.SetDrawColor(255, 0, 255, 255)
-	renderer.FillRects(rects)
-
-	renderer.Present()
-
-	sdl.Delay(2000)
+		sdl.Delay(5)
+	}
 
 	return 0
 }
